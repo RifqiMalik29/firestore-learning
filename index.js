0425b2ee263a07e983a84e8fe12ad9c75ba7d5b3
Malik <malikiskandar29@gmail.com>
2024-07-23 01:20:18 +0700

    feat: integration to list

---
+++ index.js
@@ -0,0 +1,49 @@
+// index.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBS14Pt_L6qxC8gE-s62ERLZa79z9cyaTE",
+    authDomain: "fir-firestore-learning-21ede.firebaseapp.com",
+    projectId: "fir-firestore-learning-21ede",
+    storageBucket: "fir-firestore-learning-21ede.appspot.com",
+    messagingSenderId: "698792780836",
+    appId: "1:698792780836:web:aff4cbbeaba86ecea124b1",
+    measurementId: "G-3N6G80C3L5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// Get a reference to the cafes collection
+const cafesCollection = collection(db, 'cafes');
+
+// Fetch the documents in the cafes collection
+getDocs(cafesCollection).then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        renderCafe(doc)
+    });
+}).catch((error) => {
+    console.error("Error getting documents: ", error);
+});
+
+const cafeList = document.querySelector("#cafe-list")
+
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+
+    li.appendChild(name);
+    li.appendChild(city);
+
+    cafeList.appendChild(li);
+}
